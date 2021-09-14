f61bc1db2887209240ba61c0de73a61f5f03f124
sakurajst <1015224192@qq.com>
2021-09-14 21:52:55 +0800

    生成地图
    导入了地图插件，运行GameScene场景可以看到地图。当前定位：武汉大学信息学部操场

---
+++ Assets/Mapbox/Unity/MeshGeneration/Modifiers/GameObjectModifiers/TagModifier.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable 649
+
+namespace Mapbox.Unity.MeshGeneration.Modifiers
+{
+    using UnityEngine;
+    using Mapbox.Unity.MeshGeneration.Components;
+    using Mapbox.Unity.MeshGeneration.Data;
+
+    [CreateAssetMenu(menuName = "Mapbox/Modifiers/Tag Modifier")]
+    public class TagModifier : GameObjectModifier
+    {
+        [SerializeField]
+        private string _tag;
+
+        public override void Run(VectorEntity ve, UnityTile tile)
+        {
+            ve.GameObject.tag = _tag;
+        }
+    }
+}
---
+++ Assets/Mapbox/Unity/Utilities/TelemetryConfigurationButton.cs
@@ -0,0 +1,25 @@
+﻿#pragma warning disable 649
+
+namespace Mapbox.Unity.Utilities
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	[RequireComponent(typeof(Button))]
+	public class TelemetryConfigurationButton : MonoBehaviour
+	{
+		[SerializeField]
+		bool _booleanValue;
+
+		protected virtual void Awake()
+		{
+			GetComponent<Button>().onClick.AddListener(SetPlayerPref);
+		}
+
+		void SetPlayerPref()
+		{
+			MapboxAccess.Instance.SetLocationCollectionState(_booleanValue);
+			PlayerPrefs.Save();
+		}
+	}
+}
