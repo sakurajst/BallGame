d6d34fbffebd8e72bc00cbd6b3c7058f8a1611ee
sakurajst <1015224192@qq.com>
2021-09-19 21:50:14 +0800

    小灰球的随机移动

---
+++ Assets/Scripts/Ball/AI/FindPath.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Data;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+using Mapbox.Unity;
+using System;
+using Mapbox.Directions;
+using System.Diagnostics;
+
+namespace Mapbox.Examples
+{
+    public class FindPath : MonoBehaviour
+    {
+        [SerializeField]
+        AbstractMap map;
+        Directions.Directions _directions;
+
+        [SerializeField]
+        Transform startPoint;
+        [SerializeField]
+        Transform endPoint;
+
+        void Awake()
+        {
+            _directions = MapboxAccess.Instance.Directions;
+        }
+
+        IEnumerator Start()
+        {
+            yield return new WaitForSeconds(1);
+
+            // FindPlayer(new Vector3(80, 0, -50));
+        }
+
+        public void FindTarget(Vector3 startPos, Vector3 destination, Action<List<Vector3>> callback)
+        {
+            // Debug.Log(transform.localPosition);
+            startPoint.position = startPos;
+            endPoint.position = destination;
+            ResponseHandler handler = new ResponseHandler(callback, map);
+            Query(callback, startPoint, endPoint, map, handler);
+        }
+
+        private void Query(Action<List<Vector3>> vecs, Transform start, Transform end, AbstractMap map, ResponseHandler handler)
+        {
+            Vector2d[] wp = new Vector2d[2];
+            wp[0] = start.GetGeoPosition(map.CenterMercator, map.WorldRelativeScale);
+            wp[1] = end.GetGeoPosition(map.CenterMercator, map.WorldRelativeScale);
+            DirectionResource _directionResource = new DirectionResource(wp, RoutingProfile.Walking);
+            _directionResource.Steps = true;
+
+            _directions.Query(_directionResource, handler.HandleDirectionsResponse);
+        }
+
+
+    }
+
+    class ResponseHandler
+    {
+        public Action<List<Vector3>> callback;
+        AbstractMap map;
+
+        public ResponseHandler(Action<List<Vector3>> callback, AbstractMap map)
+        {
+            this.map = map;
+            this.callback = callback;
+        }
+
+        public void HandleDirectionsResponse(DirectionsResponse response)
+        {
+            if (null == response.Routes || response.Routes.Count < 1)
+            {
+                return;
+            }
+
+            List<Vector3> dat = new List<Vector3>();
+            foreach (var point in response.Routes[0].Geometry)
+            {
+                dat.Add(Conversions.GeoToWorldPosition(point.x, point.y, map.CenterMercator, map.WorldRelativeScale).ToVector3xz());
+            }
+            callback(dat);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Ball/Hostage.cs
@@ -0,0 +1,61 @@
+﻿using Mapbox.Examples;
+using Microsoft.Win32;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 灰球：俘虏类
+/// 遇见主角或红球变为红色
+/// 移动方式：随机移动（使用寻路AI）
+/// </summary>
+public class Hostage : Ball
+{
+    [SerializeField] Vector3 direction;
+
+    FindPath findPath = null;
+    RaycastHit hit;
+
+    void Start()
+    {
+        findPath = GameObject.Find("Manager").GetComponent<FindPath>();
+        rb = transform.GetComponent<Rigidbody>();
+        StartCoroutine(RandomMove());
+    }
+
+    void Update()
+    {
+        if (Physics.SphereCast(transform.position, 1, rb.velocity, out hit, 5, 1 << 10))
+            direction += hit.normal * rb.velocity.magnitude + Vector3.Cross(hit.normal, Vector3.up).normalized;
+
+        direction += Vector3.Cross(Vector3.up, rb.velocity).normalized * Random.Range(-1f, 1f);
+
+        Roll(direction.normalized);
+    }
+
+    IEnumerator RandomMove()
+    {
+        while (true)
+        {
+            Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            findPath.FindTarget(transform.position, pos, SetPath);
+
+            yield return new WaitForSeconds(4);
+
+            foreach (Vector3 v in vecs)
+            {
+                while (true)
+                {
+                    direction += (v - transform.position).normalized;
+                    if ((v - transform.position).sqrMagnitude <= 1)
+                        break;
+                    yield return 0;
+                }
+
+                yield return 0;
+            }
+
+            yield return 0;
+        }
+    }
+}
