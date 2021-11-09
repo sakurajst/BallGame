f0bba49fca0cd75e01694ecf6cc1c871061b1889
廖梦恬 <2132321726@qq.Com>
2021-11-09 12:33:17 +0800

    完善了合体的逻辑。

---
+++ Assets/Scripts/UI/UnionArrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnionArrow : MonoBehaviour
+{
+    [SerializeField] float gapTime;
+    [SerializeField] float smallScale = 0.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeScale());
+    }
+
+    IEnumerator ChangeScale()
+    {
+        while (true)
+        {
+            for (float t = Time.time; Time.time - t < gapTime;)
+            {
+                transform.localScale = new Vector3(1, 1, 1) * Mathf.Lerp(smallScale, 1f, (float)(Time.time - t) / gapTime);
+                yield return 0;
+            }
+            for (float t = Time.time; Time.time - t < gapTime;)
+            {
+                transform.localScale = new Vector3(1, 1, 1) * Mathf.Lerp(1f, smallScale, (float)(Time.time - t) / gapTime);
+                yield return 0;
+            }
+
+            yield return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UnionButtonRing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnionButtonRing : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed = 1;
+
+    void Update()
+    {
+        transform.Rotate(0,0, rotateSpeed);
+    }
+}
